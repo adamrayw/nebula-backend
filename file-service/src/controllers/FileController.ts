653429f79b1e7b223f8d10f3a34014d3de446a32
Nabil Anugerah <nabilpangestu123@gmail.com>
2025-01-11 20:28:15 +0700

    refactor: Clean up UploadController methods and improve error handling

--- file-service/src/controllers/FileController.ts
+++ file-service/src/controllers/FileController.ts
@@ -4,110 +4,147 @@ import FileService from "../services/FileService";
 import { FilesAttributes } from "../db/models/File";
 
 class UploadController {
-    private readonly fileService: FileService;
+  private readonly fileService: FileService;
 
-    constructor() {
-        this.fileService = new FileService()
+  constructor() {
+    this.fileService = new FileService();
+  }
+
+  uploadFun = async (req: Request, res: Response) => {
+    try {
+      const file = req.file as unknown as FilesAttributes;
+      const { originalSize } = req.body;
+      const user = req.user as { id: string } | undefined;
+      file.userId = user?.id ?? "";
+      file.originalSize = Number(originalSize);
+
+      if (!req.file) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          message: "No file uploaded.",
+        });
+      }
+
+      const responseFromFiles = await this.fileService.upload(file);
+
+      if (responseFromFiles) {
+        res.status(StatusCodes.OK).json({
+          status: 200,
+          message: "Upload file successfully!",
+          data: req.file,
+        });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          status: 500,
+          message: error.message,
+        });
+      }
     }
+  };
 
-    uploadFun = async (req: Request, res: Response) => {
-        try {
-            const file = req.file as unknown as FilesAttributes;
-            const { originalSize } = req.body
-            const user = req.user as { id: string } | undefined;
-            file.userId = user?.id ?? '';
-            file.originalSize = Number(originalSize);
+  starredFiles = async (req: Request, res: Response) => {
+    try {
+      const user = req.user as { id: string } | undefined;
+      const searchQuery = req.query.s as string;
+      const offsetQuery = req.query.offset as string;
+      const token = req as unknown as { token: string };
 
-            if (!req.file) {
-                res.status(StatusCodes.BAD_REQUEST).json({
-                    message: 'No file uploaded.',
-                });
-            }
+      const { data, totalFile, lastPage } =
+        await this.fileService.getStarredFiles(
+          user?.id ?? "",
+          searchQuery,
+          offsetQuery,
+          token.token
+        );
 
-            const responseFromFiles = await this.fileService.upload(file)
-
-            if (responseFromFiles) {
-                res.status(StatusCodes.OK).json({
-                    status: 200,
-                    message: 'Upload file successfully!',
-                    data: req.file
-                })
-            }
-        } catch (error) {
-            if (error instanceof Error) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    status: 500,
-                    message: error.message
-                })
-            }
-        }
+      res.status(StatusCodes.OK).json({
+        status: 200,
+        message: "Get all starred files successfully!",
+        data,
+        totalFile: totalFile.count,
+        lastPage,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          status: 500,
+          message: error.message,
+        });
+      }
     }
+  };
 
-    getAllFiles = async (req: Request, res: Response) => {
-        try {
-            const user = req.user as { id: string } | undefined;
-            const searchQuery = req.query.s as string;
-            const offsetQuery = req.query.offset as string;
-            const token = req as unknown as { token: string }
+  getAllFiles = async (req: Request, res: Response) => {
+    try {
+      const user = req.user as { id: string } | undefined;
+      const searchQuery = req.query.s as string;
+      const offsetQuery = req.query.offset as string;
+      const token = req as unknown as { token: string };
 
-            const { data, totalFile, lastPage } = await this.fileService.getAllFiles(user?.id ?? '', searchQuery, offsetQuery, token.token)
+      const { data, totalFile, lastPage } = await this.fileService.getAllFiles(
+        user?.id ?? "",
+        searchQuery,
+        offsetQuery,
+        token.token
+      );
 
-            res.status(StatusCodes.OK).json({
-                status: 200,
-                message: "Get all files successfully!",
-                data,
-                totalFile: totalFile.count,
-                lastPage
-            })
-        } catch (error) {
-            if (error instanceof Error) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    status: 500,
-                    message: error.message
-                })
-            }
-        }
+      res.status(StatusCodes.OK).json({
+        status: 200,
+        message: "Get all files successfully!",
+        data,
+        totalFile: totalFile.count,
+        lastPage,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          status: 500,
+          message: error.message,
+        });
+      }
     }
+  };
 
-    deleteFile = async (req: Request, res: Response) => {
-        try {
-            const fileId = req.params.fileId as string;
+  deleteFile = async (req: Request, res: Response) => {
+    try {
+      const fileId = req.params.fileId as string;
 
-            const response = await this.fileService.deleteFile(fileId)
+      const response = await this.fileService.deleteFile(fileId);
 
-            res.status(StatusCodes.OK).json({
-                status: 200,
-                message: "File deleted Successfully!"
-            })
-        } catch (error) {
-            if (error instanceof Error) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    status: 500,
-                    message: error.message
-                })
-            }
-        }
+      res.status(StatusCodes.OK).json({
+        status: 200,
+        message: "File deleted Successfully!",
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          status: 500,
+          message: error.message,
+        });
+      }
     }
+  };
 
-    totalFileSize = async (req: Request, res: Response) => {
-        try {
-            const userId = req.params.userId
+  totalFileSize = async (req: Request, res: Response) => {
+    try {
+      const userId = req.params.userId;
 
-            const totalFileSize = await this.fileService.totalFileSize(userId)
+      const totalFileSize = await this.fileService.totalFileSize(userId);
 
-            res.status(StatusCodes.OK).json({
-                status: 200,
-                data: totalFileSize
-            })
-        } catch (error) {
-            if (error instanceof Error) {
-                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    status: 500,
-                    message: error.message
-                })
-            }
-        }
+      res.status(StatusCodes.OK).json({
+        status: 200,
+        data: totalFileSize,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          status: 500,
+          message: error.message,
+        });
+      }
     }
+  };
 }
 
 export default UploadController;
