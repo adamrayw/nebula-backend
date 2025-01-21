394f60061a7478f79790ea94c5140014913b589a
Adam Ray <raywibowo68@gmail.com>
2025-01-21 22:39:24 +0700

    feat: Add migrations for Payments table and related columns

---
+++ src/database/migrations/20250120144534-create-payments-table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      transaksiId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      paymentUrl: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('Payments');
+  },
+};
