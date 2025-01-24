ba93f7f67372a8d8d5cd3f58b36edfc1be5490ab
Adam Ray <raywibowo68@gmail.com>
2025-01-24 15:13:44 +0700

    feat: Refactor payment creation method to use DTO for improved data handling

--- payment-service/src/payment/payment.controller.ts
+++ payment-service/src/payment/payment.controller.ts
@@ -6,14 +6,13 @@ import {
   Param,
   Delete,
   UseInterceptors,
-  ParseUUIDPipe,
-  ParseIntPipe,
   Req,
 } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { UpdatePaymentStatusDto } from './dto/update-payment-status.dto';
 import { TransformInterceptor } from 'src/common/interceptors/transform.interceptor';
 import { ResponseMessage } from 'src/common/decorator/response-message.decorator';
+import { CreatePaymentDto } from './dto/create-payment.dto';
 
 @Controller('api/payments')
 export class PaymentController {
@@ -22,16 +21,11 @@ export class PaymentController {
   @Post()
   @UseInterceptors(TransformInterceptor)
   @ResponseMessage('Payment created successfully')
-  create(
-    @Body('userId', ParseUUIDPipe) userId: string,
-    @Body('price', ParseIntPipe) price: number,
-    @Body('type') type: string,
-    @Body('limit') limit: number,
-  ) {
-    return this.paymentService.create(userId, price, type, limit);
+  create(@Body() createPaymentDto: CreatePaymentDto) {
+    return this.paymentService.create(createPaymentDto);
   }
 
-  @Get('')
+  @Get()
   @UseInterceptors(TransformInterceptor)
   @ResponseMessage('Payments retrieved successfully')
   findAll(@Req() req) {
