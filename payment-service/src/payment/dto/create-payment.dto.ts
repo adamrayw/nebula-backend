fa3e2c6aa8de3ccf24bf306af57b3da5a9e235d8
Adam Ray <raywibowo68@gmail.com>
2025-01-24 15:13:56 +0700

    feat: Enhance CreatePaymentDto with validation for userId, price, type, and limit

--- payment-service/src/payment/dto/create-payment.dto.ts
+++ payment-service/src/payment/dto/create-payment.dto.ts
@@ -1,4 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
+
 export class CreatePaymentDto {
+  @IsUUID()
+  @IsNotEmpty()
   userId: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  limit: number;
 }
