de2bc0722f7e9b7bf4405d3dfe98240a658140f6
Adam Ray <raywibowo68@gmail.com>
2025-01-24 15:14:06 +0700

    feat: Enhance UpdatePaymentStatusDto with validation for id, order_id, and transaction_status

--- payment-service/src/payment/dto/update-payment-status.dto.ts
+++ payment-service/src/payment/dto/update-payment-status.dto.ts
@@ -1,8 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentDto } from './create-payment.dto';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class UpdatePaymentStatusDto extends PartialType(CreatePaymentDto) {
+  @IsUUID()
+  @IsNotEmpty()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   order_id: string;
+
+  @IsString()
+  @IsNotEmpty()
   transaction_status: string;
 }
