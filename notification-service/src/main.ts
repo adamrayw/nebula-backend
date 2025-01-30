f435bdb75910e4f4225bb0dc2f53d647d8b674de
Adam Ray <raywibowo68@gmail.com>
2025-01-31 03:02:39 +0700

    feat: Integrate RabbitMQ microservice for notification handling

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const microservice = app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://xdffzfle:pr60yVCQXdjLXB2MJO7GFR7ebyI8tTVZ@jaragua.lmq.cloudamqp.com/xdffzfle',
+      ],
+      queue: 'notification_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await microservice.listen();
+  console.log(`Microservice is listening`);
 }
 bootstrap();
---
+++ notification-service/src/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://xdffzfle:pr60yVCQXdjLXB2MJO7GFR7ebyI8tTVZ@jaragua.lmq.cloudamqp.com/xdffzfle',
+          ],
+          queue: 'notification_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+})
+export class RabbitmqModule {}
