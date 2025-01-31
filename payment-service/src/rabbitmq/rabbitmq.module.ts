29f6be7fc8a3ba7425013c4a0c6fef48bdea2c61
Adam Ray <raywibowo68@gmail.com>
2025-02-01 02:00:50 +0700

    feat: Create RabbitmqModule for RabbitMQ integration in payment service

---
+++ payment-service/src/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'notification_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'PAYMENT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'payment_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+})
+export class RabbitmqModule {}
