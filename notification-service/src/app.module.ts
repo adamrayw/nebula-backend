7c89865bad35882071d1834eac71e0a662e0147e
Adam Ray <raywibowo68@gmail.com>
2025-02-01 02:00:32 +0700

    feat: Integrate ConfigModule and SequelizeModule into AppModule for global configuration and database connection

--- notification-service/src/app.module.ts
+++ notification-service/src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 import { NotificationModule } from './notification/notification.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { dataBaseConfig } from './database/database.config';
 
 @Module({
-  imports: [RabbitmqModule, NotificationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot(dataBaseConfig),
+    RabbitmqModule,
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
