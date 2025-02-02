b04aa909c074d22909c1e79ad2690313ea343e98
Adam Ray <raywibowo68@gmail.com>
2025-02-02 16:32:13 +0700

    feat: Enhance notification service with CRUD operations and event handling

--- notification-service/src/notification/notification.module.ts
+++ notification-service/src/notification/notification.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { RabbitmqModule } from 'src/rabbitmq/rabbitmq.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Notification } from './entities/notification.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Notification]), RabbitmqModule],
   providers: [NotificationService],
   controllers: [NotificationController],
 })
