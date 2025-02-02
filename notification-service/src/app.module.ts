3a386185c6ec3ea0f770cfce0f50f2d7d0885651
Adam Ray <raywibowo68@gmail.com>
2025-02-02 16:31:18 +0700

    feat: Implement AuthMiddleware and global HttpExceptionFilter in notification service

--- notification-service/src/app.module.ts
+++ notification-service/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
@@ -6,6 +6,7 @@ import { NotificationModule } from './notification/notification.module';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { dataBaseConfig } from './database/database.config';
+import { AuthMiddleware } from './common/middleware/auth.middleware';
 
 @Module({
   imports: [
@@ -17,4 +18,14 @@ import { dataBaseConfig } from './database/database.config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      // .exclude({
+      //   path: 'api/payments/notification/handling',
+      //   method: RequestMethod.POST,
+      // })
+      .forRoutes('api/notification');
+  }
+}
--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 
