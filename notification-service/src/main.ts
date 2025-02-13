e98db4079a25fa646a052712b4598ce4f4dee9ce
Adam Ray <raywibowo68@gmail.com>
2025-02-13 23:37:48 +0700

    feat: Add compression middleware to notification and payment services; update package.json dependencies

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
+  app.use(compression());
   await app.listen(process.env.NOTIFICATION_PORT ?? 3000);
 
   const microservice = app.connectMicroservice<MicroserviceOptions>({
--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
+  app.use(compression());
   await app.listen(process.env.PAYMENT_PORT ?? 8083);
 
   const microservice = app.connectMicroservice<MicroserviceOptions>({
