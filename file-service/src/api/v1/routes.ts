031c109b42bccb7906a5cb64fb1b6871900c6563
Adam Ray <raywibowo68@gmail.com>
2025-03-24 20:37:50 +0700

    feat: Integrate caching middleware for file operations to enhance performance

--- file-service/src/api/v1/routes.ts
+++ file-service/src/api/v1/routes.ts
@@ -4,6 +4,7 @@ import upload from "../../middlewares/upload";
 import verifyToken from "../../middlewares/verfiyToken";
 import { uploadFileSchema } from "../../schemas/fileSchema";
 import { validateData } from "../../middlewares/validationMiddleware";
+import { cachingMiddleware, writeCache } from "../../middlewares/caching";
 
 const router = Router();
 const fileController = new FileController();
@@ -17,9 +18,9 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 // file
-router.post("/file/uploadFile", [upload, verifyToken, validateData(uploadFileSchema)], fileController.uploadFun);
-router.get("/file/getFiles", verifyToken, fileController.getAllFiles);
-router.delete("/file/deleteFile/:fileId", verifyToken, fileController.deleteFile);
+router.post("/file/uploadFile", [upload, verifyToken, validateData(uploadFileSchema), writeCache], fileController.uploadFun);
+router.get("/file/getFiles", [verifyToken, cachingMiddleware], fileController.getAllFiles);
+router.delete("/file/deleteFile/:fileId", [verifyToken, writeCache], fileController.deleteFile);
 router.get("/file/totalFileSize/:userId", verifyToken, fileController.totalFileSize);
 router.get("/file/starredFiles", verifyToken, fileController.starredFiles);
 
@@ -28,6 +29,6 @@ router.get('/file/categories', [verifyToken], fileController.getCategories)
 
 // trash
 router.get('/file/trash', [verifyToken], fileController.getTrashFile)
-router.put('/file/undoTrash/:fileId', [verifyToken], fileController.undoTrashFile)
+router.put('/file/undoTrash/:fileId', [verifyToken, writeCache], fileController.undoTrashFile)
 
 export default router;
